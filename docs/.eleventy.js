0d30656fa0f1353d3c419924d3b27eef766b1b9b
Jeff <rustyangel@gmail.com>
2021-09-15 10:41:20 -0600

    Docs: Add short code for navigation items

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,5 +1,6 @@
 const eleventyNavigationPlugin = require('@11ty/eleventy-navigation')
 const eleventyRemark = require('@fec/eleventy-plugin-remark')
+const dedent = require('dedent')
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
@@ -9,6 +10,32 @@ module.exports = function (eleventyConfig) {
         'docs/src/assets/images': './assets/images',
     })
     eleventyConfig.addPassthroughCopy({ 'docs/src/public': '.' })
+
+    eleventyConfig.addPairedShortcode(
+        'navitem',
+        function (content, url, isSelected, isInactive) {
+            let tag = ''
+
+            if (isInactive) {
+                tag = `<span class="px-3 py-2 relative block text-grey-400">${content}</span>`
+            } else {
+                let linkClass = [
+                    'px-3 py-2 transition-colors duration-200 relative block',
+                    isSelected && 'text-light-blue-700',
+                    !isSelected && 'hover:text-grey-900 text-grey-500',
+                ].join(' ')
+
+                tag = dedent`<a class="${linkClass}" href="${url}">
+                    <span class="rounded-md absolute inset-0 bg-light-blue-50 ${
+                        !isSelected && 'opacity-0'
+                    }"></span>
+                    <span class="relative">${content}</span>
+                </a>`
+            }
+
+            return `<li>${tag}</li>`
+        },
+    )
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin)
     eleventyConfig.addPlugin(eleventyRemark, {
